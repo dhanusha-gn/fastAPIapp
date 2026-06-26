c161df5d1f14b34d8dae83e88850c55a1a994589
dhanusha-gn <dhanushagn@gmail.com>
2026-06-26 16:50:01 +0530

    postgres

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
 -service --business logic
 -repository --data access layer
 -middleware -- request processing pipeline
+
+# database
+## non-realtional database
+-mongodb
+-dynamodb
+-redils
+
+# constraints in database
+-primary key --eg: student_id
+-foreign key --eg: dept_id in student table
+-unique --eg:email,phonenumber
+-not null --eg:name
+-check --eg:salary>0
+-default --eg:timestamp:func.now()
--- routers/job.py
+++ routers/job.py
@@ -1,11 +1,37 @@
 from fastapi import APIRouter
+from schemas.job import JobCreate,JobUpdate
 
 router=APIRouter(prefix="/job",tags=["job"])
+jobs=[]
+
+@router.post("/")
+def create_job(job: JobCreate):
+    jobs.append(job)
+    return job
+
+@router.get("/")
+def get_all_job():
+    return jobs
+
+@router.get("/:{job_id}")
+def get_job(job_id: int):
+    return jobs[job_id]
 
 @router.get("/")
 def read_job():
-    return{"job":"Job root"}
+    return{"Job": "Job root"}
 
 @router.get("/{job_id}")
-def read_job(job_id:int):
-    return {"job_id": job_id}
+def read_Job(job: int):
+    return {"Job_id": "job_id"}
+
+@router.put("/{job_id}")
+def update_job(job_id: int, job: JobUpdate):
+    jobs[job_id]=job
+    return jobs 
+
+@router.delete("/{job_id}")
+def delete_job(job_id: int):
+    jobs.pop(job_id)
+    return jobs
+
